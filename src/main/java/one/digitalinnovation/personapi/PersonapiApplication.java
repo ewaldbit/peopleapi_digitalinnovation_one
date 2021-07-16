9fe66b3c48ff8cf7f7c444669ff0699e87020be7
Ewald Bitencourt <ewaldbit@gmail.com>
2021-07-15 23:58:48 -0300

    Developev person create operation

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personapi;
 
+import org.apache.catalina.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class  PersonapiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PersonapiApplication.class, args);
+
 	}
 
 }
